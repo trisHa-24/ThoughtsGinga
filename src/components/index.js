139f818c17c2dd13ce1d02d05cbf3bc40e0c567f
trisHa-24 <mt123456trisha@gmail.com>
2024-09-19 11:44:22 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Select from './Select'
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from './Container/Container'
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from './Button';
+import PostCard from './PostCard';
+import PostForm from "./post-form/PostForm";
+import AuthLayout from './AuthLayout';
+import Input from './Input'
+
+export {
+    Select,
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostCard,
+    PostForm,
+    AuthLayout,
+    Input,
+}
